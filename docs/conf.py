f01ce35840e1d9866a36bedd215eb568b93e27b7
E4CRYPT3D <gohramgkb@gmail.com>
2024-02-08 23:39:40 +0500

    yaml

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys,os
+sys.path.append(os.path.abspath('..'))
+
+project = 'ScrapeMyst'
+copyright = '2024, E4CRYPT3D'
+author = 'E4CRYPT3D'
+release = '1.0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+extensions.append('sphinx.ext.todo')
+extensions.append('sphinx.ext.autodoc')
+#extensions.append('sphinx.ext.autosummary')
+# extensions.append('sphinx.ext.intersphinx')
+# extensions.append('sphinx.ext.mathjax')
+extensions.append('sphinx.ext.viewcode')
+# extensions.append('sphinx.ext.graphviz')
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
